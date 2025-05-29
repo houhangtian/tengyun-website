32bc1c3d23b9b71810f53701e84bff812e231088
houhangtian <18201027108@139.com>
2025-05-29 16:56:23 +0800

    Update base path for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: '/tengyun-website',
 }
 
 module.exports = nextConfig 
